e9766d42531aaf761a8f2d15f541b91814c5a385
Alison Williams <apw5011@gmail.com>
2020-11-23 21:51:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = []
+    sentence_array << self.split(".")
+    sentence_array << self.split("?")
+    sentence_array << self.split("!")
+    sentence_array.reject!(&:empty?)
+    if sentence_array.count > 0 
+      return sentence_array.length 
+    else 
+      return 0 
+    end 
+      
   end
 end
